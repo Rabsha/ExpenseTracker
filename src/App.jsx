095c1230fd55bbc0b22b269c2d4dd8f3c6929db7
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2024-02-22 12:12:25 +0500

    “Comments”

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ExpenseTracker from './component/Expensetracker'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ExpenseTracker />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Expensetracker.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const ExpenseTracker = () => {
+    return(
+        <div className="ExpenseTracker">RAbsha</div>
+    )
+}
+
+export default ExpenseTracker
