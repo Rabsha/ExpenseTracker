376976ad3fdde710f73f6aadece4e8c2c912b406
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2024-02-23 12:59:25 +0500

    ok

--- src/component/Expensetracker.jsx
+++ src/component/Expensetracker.jsx
@@ -11,7 +11,7 @@ const ExpenseTracker = () => {
                     <div className="row">
                         <div className="col-md-4">
                             <div className="leftsidebox">
-                                <h4>Add New Expense</h4>
+                                <h4><b>ADD NEW EXPENSE</b></h4>
                                 <form>
                                     <div className="form-group">
                                         <label>Name of Expense</label>
@@ -29,7 +29,7 @@ const ExpenseTracker = () => {
                         </div>
                         <div className="col-md-4">
                             <div className="centersidebox">
-                                <h4>History Record</h4>
+                                <h4><b>HISTORY RECORD</b></h4>
                                 <div className="myhistory">
                                     <ul>
                                         <li>Rabsha Shakeel <a href="#">Delete</a></li>
@@ -44,16 +44,16 @@ const ExpenseTracker = () => {
                         <div className="col-md-4">
                             <div className="rightsidebox">
                                 <div className="Maindata">
-                                    <h4>Balance: 45100$</h4>
+                                    <h4><b>BALANCE: 45100$</b></h4>
                                 </div>
                                 <div className="MainIncome">
                                     <div className="OneeIncome">
-                                        <h3>Income</h3>
-                                        <h3>500$</h3>
+                                        <h3 style={{color:"green",fontWeight:"bold"}}>Income</h3>
+                                        <h3 style={{color:"green",fontWeight:"bold"}}>500$</h3>
                                     </div>
                                     <div className="OneeIncome">
-                                        <h3>Outcome</h3>
-                                        <h3>500$</h3>
+                                        <h3 style={{color:"red",fontWeight:"bold"}}>Outcome</h3>
+                                        <h3 style={{color:"red",fontWeight:"bold"}}>500$</h3>
                                     </div>
                                 </div>
                             </div>
